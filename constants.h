186955cc3fd713d2c3f6a0cc24ead19cc46faec1
naman21 <nkjaipur21@gmail.com>
2015-04-19 21:11:38 +0530

    Create constants.h

---
+++ constants.h
@@ -0,0 +1,22 @@
+#ifndef CONSTANTS_H_INCLUDED
+#define CONSTANTS_H_INCLUDED
+#endif // CONSTANTS_H_INCLUDED
+
+
+
+//Frames per second(to cap the frame rate)
+const int FRAMES_PER_SECOND = 20;
+
+//Dimensions of square
+const int DOT_WIDTH = 96;
+const int DOT_HEIGHT = 96;
+
+//Dimensions of screen
+const int SCREEN_WIDTH = 1400;
+const int SCREEN_HEIGHT = 750;
+
+//Sets bits per pixel
+const int SCREEN_BPP = 32;
+
+//The colors of the font
+SDL_Color colorRed= {212,60,60}, colorWhite= {0xff,0xff,0xff};
