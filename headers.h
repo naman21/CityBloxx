22b0474aa794498d495b9819cecbbcebaa761a83
naman21 <nkjaipur21@gmail.com>
2015-04-19 21:12:50 +0530

    Create headers.h

---
+++ headers.h
@@ -0,0 +1,25 @@
+#ifndef HEADERS_H_INCLUDED
+#define HEADERS_H_INCLUDED
+
+
+
+#endif // HEADERS_H_INCLUDED
+
+#ifdef __cplusplus
+#include <cstdlib>
+#else
+#include <stdlib.h>
+#endif
+
+#include <iostream>
+#include<fstream>
+#include <string>
+
+//SDL 1.2 Library header files
+#include <SDL/SDL.h>
+#include "SDL/SDL_image.h"
+#include "SDL/SDL_ttf.h"
+#include <sstream>
+
+
+using namespace std;
